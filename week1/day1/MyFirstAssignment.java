dfa2579a07e200d3fb6a1bec92dd9facc2af0fe5
Naveen-2294 <114819168+Naveen-2294@users.noreply.github.com>
2022-10-01 20:46:27 +0530

    Add files via upload
    Week1 Day1 Assignment 
    
    My Learning from in this code:
    1. Learned about shortcuts for Print, method 
    2. Learned about For Loop function
    3. Variable Assignment

---
+++ week1/day1/MyFirstAssignment.java
@@ -0,0 +1,15 @@
+package week1.day1;
+
+public class MyFirstAssignment {
+	public static void main(String[] args) {
+		System.out.println("Factorial Value Of 5");
+		System.out.println("*********************");
+		int i =5;
+		int fact =1;
+		for(i = 1;i<=5;i++) {
+			fact = fact*i;
+		}
+		System.out.println(fact);
+	}
+
+}
